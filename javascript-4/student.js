68c5c2b18a844d327183e1fdbf79dda51f85c5c0
Grace <grace@MacBook-Pro.local>
2020-01-24 16:01:47 -0700

    skillscheck

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -4,7 +4,7 @@
 //////////////////Step 1////////////////////
 // Create a new array called 'faveColors' and assign it a value of your three favorite colors as strings.
 
-// CODE HERE
+faveColors = ["blue", "yellow", "green"]
 
 //////////////////Step 2////////////////////
 // Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, and profileImage. 
@@ -21,12 +21,18 @@
 
 // The profileImage key value should be a method that returns this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
-// CODE HERE
+let me = {
+    firstName: "Grace",
+    superHeroName: "SuperGrace",
+    homeTown: "alpine",
+    superPowerXP: ['Mega Blast', 'Thunder From Down Under', 'Calamitous Crop Duster'],
+    profileImage: `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`
 
+}
 //////////////////Step 3////////////////////
 // In this step, you will create three variables that will hold some data from your me object. The first variable you create should be 'regularName', and it should receive its value from the value of your firstName key on the me object above. The next variable is 'superName' which should have its value assigned as the value of the superHeroName property on the me object. The last variable you should create is 'homeTown' which will be the value of homeTown on the me object
 
-// CODE HERE
+
 
 //////////////////Step 4////////////////////
 //Create a function called 'setColor' that takes in one parameter called 'arr' that is an array of colors. Make sure that we only ever have three colors by using splice to trim the array to just 3 colors. 
